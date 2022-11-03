6251071f0e340c0eea8c83b2403210f66aadf291
MaverickEdwards <r.edwards0917@gmail.com>
2022-11-03 00:23:16 -0400

    commit tree 8b8e5117a31f188418cee23542ab203f462658f2

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+function multiply(num1, num2) {
+  return num1 * num2
+}
+let num1 = 2
+let num2 = 31
+multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 29
+let num4 = 5
+
+let mod = num3 % num4;
+
+let max = Math.max(20)
